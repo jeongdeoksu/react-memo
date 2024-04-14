3639ad9dc3a78ca8ecfa715c874cc757e347bcb6
jeongdeoksu <jeongdeoksu@gmail.com>
2024-04-14 00:52:51 +0000

    Add useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import Child from "./Child";
 import "./App.css";
 
 function App() {
   const [parentAge, setParentAge] = useState(0);
-  const [childAge, setChildAge] = useState(0);
 
   const incrementPerentAge = () => {
     setParentAge(parentAge + 1);
   };
 
-  const incrementChildAge = () => {
-    setChildAge(childAge + 1);
-  };
+  console.log("👪부모 컴포넌트가 렌더링이 되었어요.");
 
-  console.log('👪부모 컴포넌트가 렌더링이 되었어요.')
+  const name = useMemo(() => {
+    return {
+      lastName: "홍",
+      firstName: "길동",
+    };
+  }, []);
 
   return (
     <div style={{ border: "1px solid navy", padding: "10px" }}>
       <h1>👪부모</h1>
       <p>age: {parentAge}</p>
       <button onClick={incrementPerentAge}>부모 나이 증가</button>
-      <button onClick={incrementChildAge}>자녀 나이 증가</button>
-      <Child name={"홍길동"} age={childAge} />
+      <Child name={name} />
     </div>
   );
 }
--- src/Child.jsx
+++ src/Child.jsx
@@ -1,12 +1,12 @@
 import { memo } from "react";
 
-const Child = ({ name, age }) => {
+const Child = ({ name }) => {
   console.log("👶자녀 컴포넌트도 렌더링이 되었어요.");
   return (
     <div style={{ border: "2px solid powderblue", padding: "10px" }}>
       <h3>👶자녀</h3>
-      <p>name: {name}</p>
-      <p>age: {age}살</p>
+      <p>성: {name.lastName} </p>
+      <p>이름 : {name.firstName}</p>
     </div>
   );
 };
